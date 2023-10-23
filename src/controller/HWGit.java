fc28ea9cc850bf8a4cb83c0cf93ab092fd73df12
Wallace Pereira Sebastião Silva <wallacepssilva@gmail.com>
2023-10-23 15:20:09 -0300

    Continuação do trabalho de edição do hwGIT

--- src/controller/HWGit.java
+++ src/controller/HWGit.java
@@ -5,7 +5,8 @@ public class HWGit {
 	public static void main(String[] args) {
 		System.out.println("Olá, mundo!");
 		System.out.println("Conhecendo o GIT");
-
+//comentário
+		System.out.println("Já sei fazer commit");
 	}
 
 }
